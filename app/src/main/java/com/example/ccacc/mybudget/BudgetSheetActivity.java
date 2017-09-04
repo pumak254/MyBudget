62dd30e930878d0d09827f882a13a9e545cd086e
Cesar Chavira <cc.acct.util@gmail.com>
2017-09-03 20:21:58 -0500

    Add second activity

---
+++ app/src/main/java/com/example/ccacc/mybudget/BudgetSheetActivity.java
@@ -0,0 +1,24 @@
+package com.example.ccacc.mybudget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+public class BudgetSheetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_budget_sheet);
+
+        String[] categories = {"Rent", "Utilities", "Food"};
+        String[] budget = {"941", "111", "450"};
+
+        ListAdapter catAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, categories);
+        ListView catList = (ListView) findViewById(R.id.categories);
+        catList.setAdapter(catAdapter);
+
+    }
+}
